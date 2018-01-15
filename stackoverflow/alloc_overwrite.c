2911c72114ed40c77850a432a636f5af7f5221b1
pplonepiece@126.com <pplonepiece@126.com>
2018-01-15 09:22:30 +0800

    over write stack successful

--- stackoverflow/alloc_overwrite.c
+++ stackoverflow/alloc_overwrite.c
@@ -3,14 +3,14 @@
 
 void function(int a, int b, int c) {
 	int *ret;
-	char buffer1[5];
-	char buffer2[10];
-	ret = buffer1 + 13;
-	*(ret) += 8;
+	char buffer1[5] = {'0'};
+	char buffer2[10] = {'0'};
+	ret = buffer1 + 24;
+	*(ret) += 5;
 }
 
 void main() {
-	int i = 0;
+	int i = 10;
 	function(1, 2, 3);
 	i = 1;
 	printf("%d\n", i);
